6e591255c813f06014016c058dc7d3d79639bb88
0xyukine <44278060+0xyukine@users.noreply.github.com>
2023-07-13 22:18:27 +0100

    async queues and music playing

--- extensions/music.py
+++ extensions/music.py
@@ -2,8 +2,11 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+from async_timeout import timeout
+
 import subprocess
 import traceback
+import asyncio
 import random
 import eyed3
 import json
@@ -22,7 +25,62 @@ for dir_path, dirs, files in os.walk('/music'):
         if re.search('\.(mp3|flac)', file):
             music_files.append(f'{dir_path}/{file}')
 
+class MusicPlayer:
+    def __init__(self, ctx):
+        self.bot = ctx.client
+        self._guild = ctx.guild
+
+        self.queue = asyncio.Queue()
+        self.next = asyncio.Event()
+
+        self.np = None
+        self.volume = 1
+        self.current = None
+
+        self.counter = 1
+
+        ctx.client.loop.create_task(self.player_loop())
+
+    async def player_loop(self):
+        print("waiting until ready")
+        await self.bot.wait_until_ready()
+        print(self.queue._queue)
+        while not self.bot.is_closed():
+            print(f"loop q print{self.queue._queue}")
+            print("next clear")
+            self.next.clear()
+
+            try:
+                async with timeout(300):
+                    print("self queue get")
+                    print(f"queue before get {self.queue._queue}")
+                    source = await self.queue.get()
+                    print(source)
+                    print(f"queue after get {self.queue._queue}")
+            except asyncio.TimeoutError:
+                print("destroying")
+                return self.destroy()
+            except exception as e:
+                print("e")
+
+            print("aAAAAAAAAAAAAAaaaaaa")
+            source.volume = self.volume
+
+            self.current = source
+
+            print("playing current")
+            self._guild.voice_client.play(source, after=lambda _:self.bot.loop.call_soon_threadsafe(self.next.set))
+            print("next wait")
+            await self.next.wait()
+
+            self.current = None
+
 class MyGroup(app_commands.Group):
+    @app_commands.command()
+    async def test(self, ctx):
+        self.player = MusicPlayer(ctx)
+        await ctx.response.send_message("creating player", delete_after=20)
+
     @app_commands.command()
     async def join(self, ctx, *, input_channel: discord.VoiceChannel = None):
         if ctx.guild.voice_client is None:
@@ -44,40 +102,25 @@ class MyGroup(app_commands.Group):
 
     @app_commands.command()
     async def play(self, ctx, music_search: str = None):
-        async def play_random_song(self, ctx, music_search):
-            temp_list = []
-            if music_search:
-                for file in music_files:
-                    if re.search(f'.*{music_search}.*', file, flags=re.IGNORECASE):
-                        temp_list.append(file)
-            
-            if not temp_list:
-                temp_list = music_files
+        temp_list = []
+        if music_search:
+            for file in music_files:
+                if re.search(f'.*{music_search}.*', file, flags=re.IGNORECASE):
+                    temp_list.append(file)
+        
+        if not temp_list:
+            temp_list = music_files
 
-            song = random.choice(temp_list)
-            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song))
-            ctx.guild.voice_client.play(source)
-
-            try:
-                #Excepts if audio file doesn't have a thumbnail
-                thumb_check = subprocess.run(['ffmpeg', '-y', '-i', song, '-an', '-c:v', 'copy', '/temp/thumbnail.jpg'], text=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                thumbnail = discord.File('/temp/thumbnail.jpg', filename='thumb.jpg')
-            except subprocess.CalledProcessError:
-                thumbnail = discord.File('/mnt/e/Stuff/dfn.jpg', filename='thumb.jpg')
-
-            #Get audio duration from ffprobe output
-            pattern = re.compile("Duration:\s*([^\n\r]*[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2})")
-            song_info = str(subprocess.run(['ffprobe','-i', song], capture_output=True, text=True))
-            song_length = ":".join(re.search(pattern, song_info).group(1).split(":")[1:])
-
-            embed = discord.Embed(title=os.path.split(song)[-1], description=f'00:00 / {song_length}')
-            embed = embed.set_author(name='Nagant Player')
-            embed = embed.set_thumbnail(url='attachment://thumb.jpg')
-            embed = embed.set_footer(text=song)
-
-            await ctx.response.send_message(file=thumbnail, embed=embed)
-
-        await play_random_song(self, ctx, music_search)
+        song = random.choice(temp_list)
+        print(song)
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song))
+        await self.player.queue.put(source)
+        print(f"play q print{self.player.queue._queue}")
+        await ctx.response.send_message("adding to queue", delete_after=20)
+    
+    @app_commands.command()
+    async def queue_info(self, ctx):
+        await ctx.response.send_message(list(self.player.queue._queue), delete_after=20)
             
     @app_commands.command()
     async def pause(self, ctx):
